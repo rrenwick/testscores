60ffa3992fc7b55f3e5c036e4e30c83dfb324987
Rob <robjrenwick@gmail.com>
2016-06-01 15:06:00 +1000

    image alignment and bleed using jquery

--- functions.php
+++ functions.php
@@ -139,7 +139,7 @@ function testscores_scripts() {
         // Add Font Awesome icons (http://fontawesome.io) 
 	wp_enqueue_style( 'popperscores-fontawesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css' );
         
-               	wp_enqueue_script( 'testscores-navigation', get_template_directory_uri() . '/js/navigation.js', array( 'jquery'), '20151215', true );
+               	wp_enqueue_script( 'testscores-navigation', get_template_directory_uri() . '/js/functions.js', array( 'jquery'), '20151215', true );
                  wp_localize_script( 'testscores-navigation', 'screenReaderText', array(
 		'expand'   => '<span class="screen-reader-text">' . __( 'expand child menu', 'testscores' ) . '</span>',
 		'collapse' => '<span class="screen-reader-text">' . __( 'collapse child menu', 'testscores' ) . '</span>',
--- js/functions.js
+++ js/functions.js
@@ -134,5 +134,7 @@
 		position = $(this).scrollTop();
 	});
 
+            // Wrap centered images in a new figure element
+	$( 'img.aligncenter' ).wrap( '<figure class="centered-image"></figure>');
 
 } )( jQuery );
