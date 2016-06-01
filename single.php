cf39ea4eea6aa3b69597435f227cab81a3512db9
Rob <robjrenwick@gmail.com>
2016-06-02 07:32:09 +1000

    styled and modified post navigation links

--- single.php
+++ single.php
@@ -23,7 +23,14 @@ get_header(); ?>
 				comments_template();
 			endif;
 
-			the_post_navigation();
+			the_post_navigation( array(
+				'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'popperscores' ) . '</span> ' .
+					'<span class="screen-reader-text">' . __( 'Next post:', 'popperscores' ) . '</span> ' .
+					'<span class="post-title">%title</span>',
+				'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'popperscores' ) . '</span> ' .
+					'<span class="screen-reader-text">' . __( 'Previous post:', 'popperscores' ) . '</span> ' .
+					'<span class="post-title">%title</span>',
+			) );
                         
 		endwhile; // End of the loop.
 		?>
