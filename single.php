34239dcfa796596455b665dedc63114a3dc09b88
Rob <robjrenwick@gmail.com>
2016-06-03 10:31:16 +1000

    slight modification to domain

--- single.php
+++ single.php
@@ -24,11 +24,11 @@ get_header(); ?>
 			endif;
 
 			the_post_navigation( array(
-				'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'popperscores' ) . '</span> ' .
-					'<span class="screen-reader-text">' . __( 'Next post:', 'popperscores' ) . '</span> ' .
+				'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'testscores' ) . '</span> ' .
+					'<span class="screen-reader-text">' . __( 'Next post:', 'testscores' ) . '</span> ' .
 					'<span class="post-title">%title</span>',
-				'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'popperscores' ) . '</span> ' .
-					'<span class="screen-reader-text">' . __( 'Previous post:', 'popperscores' ) . '</span> ' .
+				'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'testscores' ) . '</span> ' .
+					'<span class="screen-reader-text">' . __( 'Previous post:', 'testscores' ) . '</span> ' .
 					'<span class="post-title">%title</span>',
 			) );
                         
