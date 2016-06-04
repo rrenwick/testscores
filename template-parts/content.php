83d7f80c07ba667d7eb525a4cc99c42e0128e546
Rob <robjrenwick@gmail.com>
2016-06-04 10:48:38 +1000

    added support for featured images to content template

--- template-parts/content.php
+++ template-parts/content.php
@@ -11,7 +11,16 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php
+		
+                                    <?php 
+		if ( has_post_thumbnail() ) { ?>
+			<figure class="featured-image">
+				<?php the_post_thumbnail(); ?>
+			</figure>
+		<?php }
+		?>
+            
+                                    <?php
 			if ( is_single() ) {
 				the_title( '<h1 class="entry-title">', '</h1>' );
 			} else {
